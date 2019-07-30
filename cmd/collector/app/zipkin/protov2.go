7d4e589fb80993510057e0c5379b4190c102c56e
Abhilash Gnan <abhilashgnan@gmail.com>
2019-07-30 20:13:12 +0200

    WIP proto to thrift convertor
    Signed-off-by: Abhilash Gnan <abhilashgnan@gmail.com>

--- cmd/collector/app/zipkin/protov2.go
+++ cmd/collector/app/zipkin/protov2.go
@@ -1,16 +1,15 @@
 package zipkin
 
 import (
-	"time"
-
+	zmodel "github.com/jaegertracing/jaeger/proto-gen/zipkin"
 	"github.com/jaegertracing/jaeger/thrift-gen/zipkincore"
 	"github.com/openzipkin/zipkin-go/model"
 )
 
 // Converts Zipkin Protobuf spans to Thrift model
-func protoSpansV2ToThrift(zss []*model.SpanModel) ([]*zipkincore.Span, error) {
-	tSpans := make([]*zipkincore.Span, 0, len(zss))
-	for _, span := range zss {
+func protoSpansV2ToThrift(listOfSpans *zmodel.ListOfSpans) ([]*zipkincore.Span, error) {
+	tSpans := make([]*zipkincore.Span, 0, len(listOfSpans.Spans))
+	for _, span := range listOfSpans.Spans {
 		tSpan, err := protoSpanV2ToThrift(span)
 		if err != nil {
 			return nil, err
@@ -20,47 +19,55 @@ func protoSpansV2ToThrift(zss []*model.SpanModel) ([]*zipkincore.Span, error) {
 	return tSpans, nil
 }
 
-func protoSpanV2ToThrift(s *model.SpanModel) (*zipkincore.Span, error) {
-	sc := s.SpanContext
-	ts := nanoToMicroSecs(s.Timestamp.UnixNano())
-	d := nanoToMicroSecs(s.Duration.Nanoseconds())
+func protoSpanV2ToThrift(s *zmodel.Span) (*zipkincore.Span, error) {
+	id, err := model.SpanIDFromString(cutLongID(*s.ID))
+	if err != nil {
+		return nil, err
+	}
+	traceID, err := model.TraceIDFromString(*s.TraceID)
+	if err != nil {
+		return nil, err
+	}
 	tSpan := &zipkincore.Span{
-		ID:        int64(sc.ID),
-		TraceID:   int64(sc.TraceID.Low),
+		ID:        int64(id),
+		TraceID:   int64(traceID.Low),
 		Name:      s.Name,
 		Debug:     s.Debug,
-		Timestamp: &ts,
-		Duration:  &d,
+		Timestamp: &s.Timestamp,
+		Duration:  &s.Duration,
 	}
-	if sc.TraceID.High != 0 {
-		help := int64(sc.TraceID.High)
+	if traceID.High != 0 {
+		help := int64(traceID.High)
 		tSpan.TraceIDHigh = &help
 	}
 
-	if len(sc.ParentID.String()) > 0 {
-		signed := int64(*sc.ParentID)
+	if len(s.ParentID) > 0 {
+		parentID, err := model.SpanIDFromString(cutLongID(s.ParentID))
+		if err != nil {
+			return nil, err
+		}
+		signed := int64(parentID)
 		tSpan.ParentID = &signed
 	}
 
 	var localE *zipkincore.Endpoint
 	if s.LocalEndpoint != nil {
-		var err error
-		localE, err = protoEndpointV2ToThrift(s.LocalEndpoint)
+		localE, err = endpointV2ToThrift(s.LocalEndpoint)
 		if err != nil {
 			return nil, err
 		}
 	}
 
 	for _, a := range s.Annotations {
-		tA := protoAnnoV2ToThrift(&a, localE)
+		tA := annoV2ToThrift(a, localE)
 		tSpan.Annotations = append(tSpan.Annotations, tA)
 	}
 
 	tSpan.BinaryAnnotations = append(tSpan.BinaryAnnotations, tagsToThrift(s.Tags, localE)...)
-	tSpan.Annotations = append(tSpan.Annotations, kindToThrift(ts, d, string(s.Kind), localE)...)
+	tSpan.Annotations = append(tSpan.Annotations, kindToThrift(s.Timestamp, s.Duration, s.Kind, localE)...)
 
 	if s.RemoteEndpoint != nil {
-		rAddrAnno, err := protoRemoteEndpToThrift(s.RemoteEndpoint, s.Kind)
+		rAddrAnno, err := remoteEndpToThrift(s.RemoteEndpoint, s.Kind)
 		if err != nil {
 			return nil, err
 		}
@@ -77,46 +84,4 @@ func protoSpanV2ToThrift(s *model.SpanModel) (*zipkincore.Span, error) {
 		})
 	}
 	return tSpan, nil
-}
-
-func protoEndpointV2ToThrift(e *model.Endpoint) (*zipkincore.Endpoint, error) {
-	if e == nil {
-		return nil, nil
-	}
-	return eToThrift(string(e.IPv4), string(e.IPv6), int32(e.Port), e.ServiceName)
-}
-
-func protoAnnoV2ToThrift(a *model.Annotation, e *zipkincore.Endpoint) *zipkincore.Annotation {
-	ts := nanoToMicroSecs(a.Timestamp.UnixNano())
-	return &zipkincore.Annotation{
-		Value:     a.Value,
-		Timestamp: ts,
-		Host:      e,
-	}
-}
-
-func protoRemoteEndpToThrift(e *model.Endpoint, kind model.Kind) (*zipkincore.BinaryAnnotation, error) {
-	rEndp, err := protoEndpointV2ToThrift(e)
-	if err != nil {
-		return nil, err
-	}
-	var key string
-	switch kind {
-	case model.Server:
-		key = zipkincore.SERVER_ADDR
-	case model.Client:
-		key = zipkincore.CLIENT_ADDR
-	case model.Consumer, model.Producer:
-		key = zipkincore.MESSAGE_ADDR
-	}
-
-	return &zipkincore.BinaryAnnotation{
-		Key:            key,
-		Host:           rEndp,
-		AnnotationType: zipkincore.AnnotationType_BOOL,
-	}, nil
-}
-
-func nanoToMicroSecs(ns int64) int64 {
-	return ns / int64(time.Millisecond)
 }
